d033c1b3754e089bf674085d7c262f7995d53981
Abdulahad-xd <ahada8048@gmail.com>
2023-09-08 16:48:40 +0500

    some re-arrangements to make this code work

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { Box } from './components/box'
+import WeatherComponent from './components/functionality'
+
+export default function Home() {
+  return (
+    <div>
+   |<Box/>
+    
+    </div>
+    )
+}
